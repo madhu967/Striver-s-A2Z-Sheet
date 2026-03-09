4849673febe142be6b18267d92f80642ce79d510
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-03-09 20:42:48 +0530

    Update 12.MoveZerosToEnd.java

--- Arrays/12.MoveZerosToEnd.java
+++ Arrays/12.MoveZerosToEnd.java
@@ -16,3 +16,26 @@ class Solution {
         }
     }
 }
+
+//optimla approch
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int j=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return;
+        for(int i=j+1;i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                j++;
+            }
+        }
+    }
+}
