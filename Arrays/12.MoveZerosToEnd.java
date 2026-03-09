638c849bb878468e57c1641285946dfa0cf8fbb2
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-03-09 20:21:31 +0530

    Create 12.MoveZerosToEnd.java

---
+++ Arrays/12.MoveZerosToEnd.java
@@ -0,0 +1,18 @@
+//better apprcoh
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int n=arr.length;
+        int[] temp=new int[n];
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                temp[j]=arr[i];
+                j++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=temp[i];
+        }
+    }
+}
