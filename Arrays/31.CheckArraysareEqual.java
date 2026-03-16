76a42d0d65323ee8da77b59e44b2058b7c1db90c
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-03-16 18:24:53 +0530

    Create 31.CheckArraysareEqual.java

---
+++ Arrays/31.CheckArraysareEqual.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static boolean checkEqual(int[] a, int[] b) {
+        // code here
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if(a.length!=b.length) return false;
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
