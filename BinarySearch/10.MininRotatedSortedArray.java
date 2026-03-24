b32b0de72cf767282568ef2381fdd225120da54a
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-03-24 06:46:48 +0530

    Create 10.MininRotatedSortedArray.java

---
+++ BinarySearch/10.MininRotatedSortedArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMin(int[] arr) {
+        // code here
+        int low=0,high=arr.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>arr[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return arr[low];
+        
+    }
+}
